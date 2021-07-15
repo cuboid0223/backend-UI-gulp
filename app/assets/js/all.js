ce4d99658da27ad9b6fe9d3e2f1db71579914b0a
Cuboid <yyytaiwanzed@gmail.com>
2021-07-15 19:38:56 +0800

    v1.0

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,8 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(function () {
+  console.log("Hello Bootstrap5");
+
+  //   display none the backdrop of the replyModal
+  $("#replyModal").modal({
+    backdrop: false,
+  });
 });
